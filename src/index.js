d0591d575f3c996383730a82c0cefa7b19b348f3
christine.pham <cstinepham@gmail.com>
2019-01-11 12:26:26 -0800

    Implement random keys showing up

--- src/index.js
+++ src/index.js
@@ -6,17 +6,14 @@ var tile1, tile2, tile3, tile4;
 function drawCanvas() {
   canvasElement = document.getElementById("myCanvas");
 
-  canvasElement.width = 480;
-  canvasElement.height = 600;
   CANVAS_WIDTH = canvasElement.width;
   CANVAS_HEIGHT = canvasElement.height;
-  console.log("CANVAS_HEIGHT: "+ CANVAS_HEIGHT);
-  console.log("CANVAS_WIDTH: " + CANVAS_WIDTH);
+  
   
 
   canvasCtx = canvasElement.getContext("2d");
   canvasCtx.fillStyle = "000000";
-  //canvasCtx.fillRect(0, 20, 150, 100);
+  
   var xt = Math.floor(Math.random() * 4);
   tile1 = xt;
   xt = Math.floor(Math.random() * 4);
@@ -34,12 +31,16 @@ function drawCanvas() {
 }
 
 var filledTile = {
-  color: "000000",
+  color: "#000000",
   x: 0,
   y: 0,
-  width: CANVAS_WIDTH/4,
-  height: CANVAS_WIDTH/4,
+  width: 480/4,
+  height: 600/4,
   draw: function(z1, z2) {
+    console.log("CANVAS_HEIGHT: " + CANVAS_HEIGHT);
+    console.log("CANVAS_WIDTH: " + CANVAS_WIDTH);
+    console.log("width is: " + this.width)
+    console.log("height is: " + this.height);
     canvasCtx.fillStyle = this.color;
     canvasCtx.fillRect(z1 * this.width, z2 * this.height, this.width, this.height);
   },
@@ -59,20 +60,25 @@ var filledTile = {
 
 function drawLines() {
   canvasCtx.beginPath();
-  canvasCtx.moveTo(CANVAS_WIDTH / 4, 0);
-  canvasCtx.lineTo(CANVAS_WIDTH / 4, CANVAS_HEIGHT);
-  canvasCtx.moveTo(2 * CANVAS_WIDTH / 4, 0);
-  canvasCtx.lineTo(2 * CANVAS_WIDTH / 4, CANVAS_HEIGHT);
-  canvasCtx.moveTo(3 * CANVAS_WIDTH / 4, 0);
-  canvasCtx.lineTo(3 * CANVAS_WIDTH / 4, CANVAS_HEIGHT);
-  canvasCtx.moveTo(0, CANVAS_HEIGHT / 4);
-  canvasCtx.lineTo(CANVAS_WIDTH, CANVAS_HEIGHT / 4);
-  canvasCtx.moveTo(0, 2 * CANVAS_HEIGHT / 4);
-  canvasCtx.lineTo(CANVAS_WIDTH, 2 * CANVAS_HEIGHT / 4);
-  canvasCtx.moveTo(0, 3 * CANVAS_HEIGHT / 4);
-  canvasCtx.lineTo(CANVAS_WIDTH, 3 * CANVAS_HEIGHT / 4);
+  // Vertical Lines
+  for(let i = 1; i < 4; i++) {
+    canvasCtx.moveTo(i * CANVAS_WIDTH/4, 0);
+    canvasCtx.lineTo(i * CANVAS_WIDTH / 4, CANVAS_HEIGHT);
+  }
+  
+  // Horizontal Lines
+  for (let i = 1; i < 4; i++) {
+    canvasCtx.moveTo(0, i * CANVAS_HEIGHT / 4);
+    canvasCtx.lineTo(CANVAS_WIDTH, i * CANVAS_HEIGHT / 4);  
+  }
   canvasCtx.stroke();
 }
+
+function draw() {
+  var xt = Math.floor(Math.random() * 3);
+  var yt = Math.floor(Math.random() * 3);
+}
+
 document.querySelector(".playGameButton").addEventListener("click", drawCanvas)
 
 console.log("Webpack is working =)")
