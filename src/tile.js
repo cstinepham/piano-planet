97d36249ad352710cb8e65cc526273f31188a2f3
christine.pham <cstinepham@gmail.com>
2019-01-28 23:00:22 -0800

    refactor tile class

--- src/tile.js
+++ src/tile.js
@@ -1,6 +1,13 @@
 class Tile {
-  constructor() {
+  constructor(width, height) {
     this.color = "#000";
-    
+    this.x = 0;
+    this.y = 0;
+    this.width = width/4;
+    this.height = height/4;
+  }
+  
+  draw(z1, z2, conte) {
+
   }
 }
